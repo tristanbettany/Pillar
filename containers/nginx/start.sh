de3fd2d3e5db1bf394b9f5df4ec45680367a031b
Tristan Bettany <tristan.bettany@gmail.com>
2021-09-11 16:42:02 +0100

    Add mailhog container

--- containers/nginx/start.sh
+++ containers/nginx/start.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-openssl req -x509 -new -nodes -days 365 \
- -keyout /ca/pillar.key \
- -out /ca/pillar.crt \
- -config /etc/nginx/ssl.cnf
+if [ ! -f /ca/pillar.crt ]; then
+  openssl req -x509 -new -nodes -days 365 \
+   -keyout /ca/pillar.key \
+   -out /ca/pillar.crt \
+   -config /etc/nginx/ssl.cnf
+fi
 
 exec nginx -g 'daemon off;'
