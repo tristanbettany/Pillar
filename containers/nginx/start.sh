9e012f13768241e0f622efdf650b087609d89b63
Tristan Bettany <tristan.bettany@gmail.com>
2021-11-15 13:13:34 +0000

    Update SSL for linux, this wont work in windows either

--- containers/nginx/start.sh
+++ containers/nginx/start.sh
@@ -1,10 +1,42 @@
 #!/bin/bash
 
-if [ ! -f /ca/pillar.crt ]; then
-  openssl req -x509 -new -nodes -days 365 \
-   -keyout /ca/pillar.key \
-   -out /ca/pillar.crt \
-   -config /etc/nginx/ssl.cnf
+if [ ! -f /ca/pillar-ca.key ]; then
+  openssl genrsa \
+      -des3 \
+      -out /ca/pillar-ca.key \
+      -passout pass:pillar 2048 \
+
+    openssl req \
+      -x509 \
+      -new \
+      -nodes \
+      -key /ca/pillar-ca.key \
+      -sha256 \
+      -days 365 \
+      -subj '/CN=Pillar/O=Pillar/C=GB' \
+      -out /ca/pillar-ca.pem \
+      -passin pass:pillar
 fi
+
+openssl genrsa \
+  -out /ca/pillar.key 2048
+
+openssl req \
+  -new \
+  -key /ca/pillar.key \
+  -subj '/CN=Pillar/O=Pillar/C=GB' \
+  -out /ca/pillar.csr
+
+openssl x509 \
+  -req \
+  -in /ca/pillar.csr \
+  -CA /ca/pillar-ca.pem \
+  -CAkey /ca/pillar-ca.key \
+  -CAcreateserial \
+  -out /ca/pillar.crt \
+  -days 365 \
+  -sha256 \
+  -extfile /etc/nginx/ssl.cnf \
+  -passin pass:pillar
 
 exec nginx -g 'daemon off;'
